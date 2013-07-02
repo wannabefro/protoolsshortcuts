9387f6df2def47a608e5a4a591dc138074f40df9
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-07-02 11:56:36 -0400

    search fully working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,4 +4,10 @@ function ProtoolsCtrl($scope, $http) {
   $http.get('shortcuts/protools.json').success(function(data){
     $scope.shortcuts = data;
   });
+
+  $scope.select = function(shortcut) {
+  $scope.selected = shortcut;
+  };
 }
+
+
