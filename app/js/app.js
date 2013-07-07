235cba300afeff78bcfdeff176fe52ef6e5d4d2d
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-07-07 13:33:26 -0400

    load js files last

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,6 @@
 angular.module('protools', []).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-      when('/shortcuts', {templateUrl: 'partials/protools-shortcuts.html', controller: ProtoolsCtrl}).
-      otherwise({redirectTo: '/shortcuts'});
+      when('/', {templateUrl: 'partials/protools-shortcuts.html', controller: ProtoolsCtrl}).
+      otherwise({redirectTo: '/'});
   }]);
